c82eb86997b1264192a3fc240da88e271831fd4e
Alex Lu <alex.lu@hp.com>
2026-03-02 00:23:38 +0800

    add gRCP note

--- gRPC_practive/client/client.py
+++ gRPC_practive/client/client.py
@@ -11,14 +11,12 @@ import hello_pb2_grpc
 def run():
     print("Will try to greet world ...")
     # Connect to the server on localhost port 50051
-    # If running inside docker and testing from outside, use localhost
-    # Make sure to run this script where it can reach the port
     target = os.getenv('GRPC_SERVER_TARGET', 'localhost:50051')
     print(f"Connecting to {target}...")
     
     with grpc.insecure_channel(target) as channel:
         stub = hello_pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(hello_pb2.HelloRequest(name='Practice User'))
+        response = stub.SayHello(hello_pb2.HelloRequest(name='Local Client User'))
     print("Greeter client received: " + response.message)
 
 if __name__ == '__main__':
--- gRPC_practive/request.md
+++ gRPC_practive/request.md
@@ -6,4 +6,7 @@ code 都寫在 qRPC_practive/
 3. server_A using python and uv and gRPC package, have simple end point
 
 
+1. 我預期 grpc_practive-server_a 有個endpoint
+2. 我用 client.py 去 call server_A 的 endpoint
+3. client.py代表前端 在我Local 可以執行 也幫local 裝 UV
 
